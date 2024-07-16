c9e566b5dade00a6663d157f147a2c7839984cdd
nicolas <nicordenes@gmail.com>
2024-07-16 13:40:26 -0400

    refactor: change app router to BrowserRouter

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
 function Root() {
   return (
     <>
       <h1>Planeshift</h1>
+      <h2>Games</h2>
+      <ul>
+        <li>
+          <Link to="planechase">Planechase</Link>
+        </li>
+        <li>
+          <Link to="blind-eternities">Blind Eternities Map</Link>
+        </li>
+      </ul>
+      <h2>Planes</h2>
+      <Link to="/planes">Planes</Link>
     </>
   );
 }
